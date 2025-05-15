c9eb4e313f0e938058f86aec5ee944191d990b48
hsrokon <hsrokonofficial@gmail.com>
2025-05-15 22:56:44 +0600

    category buttons activated in colors

--- src/components/sub/CouponAside.jsx
+++ src/components/sub/CouponAside.jsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import { CouponCatContext } from "../main/Coupons";
+import { NavLink } from "react-router-dom";
 
 const CouponAside = () => {
 
@@ -12,7 +13,7 @@ const CouponAside = () => {
     },[])
 
     //context
-    const { setCat } = useContext(CouponCatContext);
+    const { cat: activeCat, setCat } = useContext(CouponCatContext);
     const handleSetCategory = cat => {
         setCat(cat);
     }
@@ -24,8 +25,9 @@ const CouponAside = () => {
                 categories.map((cat, idx)=>
             <div 
             onClick={()=> handleSetCategory(cat)}
-            key={idx} className="bg-white w-full flex items-center justify-center">
-                <button className="px-6 py-2 font-medium bg-[#4FC3F7] text-white w-full transition-all shadow-[3px_3px_0px_#FF6B2C] hover:shadow-none hover:translate-x-[3px] hover:translate-y-[3px]">
+            key={idx} className="bg-white w-full flex items-center justify-center"> 
+                <button className={`px-6 py-2 font-medium 
+                    ${activeCat===cat ? 'bg-primary shadow-[3px_3px_0px_#4FC3F7]':'bg-[#4FC3F7] shadow-[3px_3px_0px_#FF6B2C]'}  text-white w-full transition-all  hover:shadow-none hover:translate-x-[3px] hover:translate-y-[3px]`}>
                     {cat}
                 </button>
             </div>

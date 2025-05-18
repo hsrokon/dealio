1e6d0d35748dbc43ccf7b60fb567b55e6ecc2f26
hsrokon <hsrokonofficial@gmail.com>
2025-05-18 12:50:56 +0600

    dashboard made responsive

--- src/components/main/Dashboard.jsx
+++ src/components/main/Dashboard.jsx
@@ -37,26 +37,26 @@ const Dashboard = () => {
     }
 
     return (
-        <div className="min-h-screen w-10/12 mx-auto grid grid-cols-10 my-10">
-            <div className="col-span-6">
-                <h1 className="text-3xl">Hey, <span className="text-primary">{user?.displayName}</span> hope you're doing great!</h1>
-                <p className="mt-4">&#10095; Email: <span className="font-semibold text-primary">{user?.email}</span></p>
-                <div className="w-96 h-80 mt-4">
+        <div className="min-h-screen w-10/12 mx-auto lg:grid lg:grid-cols-10 my-10">
+            <div className="lg:col-span-6">
+                <h1 className="text-2xl lg:text-3xl">Hey, <span className="text-primary">{user?.displayName}</span> hope you're doing great!</h1>
+                <p className="mt-4 text-sm lg:text-base">&#10095; Email: <span className="font-semibold text-primary">{user?.email}</span></p>
+                <div className="md:w-96 md:h-80 mt-4">
                     <img className="w-full h-full object-cover" src={user?.photoURL} alt="" />
                 </div>
-                <div className="mt-10">
-                    <Link to={'/saved'} className="text-xl font-semibold hover:text-primary hover:underline">&#10095; View your saved coupons</Link>
+                <div className="mt-3 md:mt-5 lg:mt-10">
+                    <Link to={'/saved'} className="md:text-xl font-semibold hover:text-primary hover:underline">&#10095; View your saved coupons</Link>
                 </div>
-                <div className="mt-10">
-                    <Link to={'/coupons'} className="text-xl font-semibold hover:text-primary hover:underline">&#10095; View coupons</Link>
+                <div className="mt-3 md:mt-5 lg:mt-10">
+                    <Link to={'/coupons'} className="md:text-xl font-semibold hover:text-primary hover:underline">&#10095; View coupons</Link>
                 </div>
-                <div className="mt-10">
-                    <Link to={'/'} className="text-xl font-semibold hover:text-primary hover:underline">&#10095; Go home</Link>
+                <div className="mt-3 md:mt-5 lg:mt-10">
+                    <Link to={'/'} className="md:text-xl font-semibold hover:text-primary hover:underline">&#10095; Go home</Link>
                 </div>
             </div>
-            <div className="col-span-4">
+            <div className="lg:col-span-4 mt-8 md:mt-12 lg:mt-0">
                 <h2 className="text-2xl">&#10095; Update your profile</h2>
-                <div className="mt-4">
+                <div className="lg:mt-4">
                     <form onSubmit={handleSubmit} className="card-body">
                     <fieldset className="fieldset">
 
@@ -75,7 +75,7 @@ const Dashboard = () => {
                     placeholder="Profile photo URL" />
                     
         
-                    <button className="btn btn-primary text-white border-base-100 rounded-2xl mt-4">Update profile</button>
+                    <button className="btn btn-primary text-white border-base-100 rounded-2xl mt-2 md:mt-4">Update profile</button>
                     </fieldset>
                 </form>
                 </div>
